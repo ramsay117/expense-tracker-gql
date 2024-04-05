7c07f1c3be6109f30b53ca8a0222859dfececf31
ramsay117 <11shubhamgupta99@gmail.com>
2024-04-05 06:39:38 +0530

    Add UI design

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import TransactionPage from './pages/TransactionPage';
+import NotFound from './pages/NotFoundPage';
+import Header from './components/ui/Header';
+import { BackgroundGradient } from './components/ui/background-gradient.jsx';
+
+function App() {
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
