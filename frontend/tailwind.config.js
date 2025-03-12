72c2f3b1e580c7b6b7bed854cc412c58b1ce6cfd
ramsay117 <11shubhamgupta99@gmail.com>
2025-03-12 16:13:02 +0530

    refactor: Remove unused background grid styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,8 +8,6 @@ export default {
     extend: {
       backgroundImage: {
         'grid-white': 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 32 32\' width=\'32\' height=\'32\' fill=\'none\' stroke=\'%23FFFFFF22\'%3E%3Cpath d=\'M0 .5H31.5V32\'/%3E%3C/svg%3E")',
-        'grid-black': 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 32 32\' width=\'32\' height=\'32\' fill=\'none\' stroke=\'%23000000\'%3E%3Cpath d=\'M0 .5H31.5V32\'/%3E%3C/svg%3E")',
-        'dot-white': 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 32 32\' width=\'16\' height=\'16\' fill=\'none\'%3E%3Ccircle fill=\'%23FFFFFF22\' id=\'pattern-circle\' cx=\'10\' cy=\'10\' r=\'1.6257413380501518\'%3E%3C/circle%3E%3C/svg%3E")',
       },
     },
   },
